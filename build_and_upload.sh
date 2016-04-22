771605d82b702201a51cdc29a0ba536d56918df9
tfrank <tobias.frank@immobilienscout24.de>
2016-04-22 10:52:41 +0200

    remove ping

--- build_and_upload.sh
+++ build_and_upload.sh
@@ -7,8 +7,6 @@ source pypi_conf.sh
 
 cd dist
 
-ping -c 5 $PYPI_INDEX_URL
-
 pip install devpi-client --index=$PYPI_INDEX_URL
 devpi use $PYPI_INDEX_URL
 devpi login $PYPI_USER --password=$PYPI_PASSWORD
